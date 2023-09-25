0cc93c2eb76e06d1315a47d030cd8acbf9d763cf
Sarah <sabrawdy@gmail.com>
2023-09-25 15:03:58 -0500

    fixed errors with imports

---
+++ src/Nav.js
@@ -0,0 +1,16 @@
+function nav() {
+    return (
+        <nav>
+            <img src="public/Images/Logo.svg" alt="LittLe Lemon logo" />
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Reservations</li>
+                <li>Order Online</li>
+                <li>Login</li>
+            </ul>
+        </nav>
+   );
+};
+
+export default nav
